fe878d3c65c74fa61cead6d8a3f485c5a3487528
katanavenus@gmail.com <katanavenus@gmaiol.com>
2026-08-20 09:43:07 +0300

    Fix file casing for vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ShopProvider } from './ShopContext'
+import { ShopProvider } from './ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
